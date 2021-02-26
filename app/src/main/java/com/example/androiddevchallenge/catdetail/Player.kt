c33faf4be5facde6be0c45b1595055d915eb80c2
Patrizio Lerede <patrizio.lerede@skytv.it>
2021-02-26 18:35:52 +0100

    spotless lint

--- app/src/main/java/com/example/androiddevchallenge/catdetail/Player.kt
+++ app/src/main/java/com/example/androiddevchallenge/catdetail/Player.kt
@@ -1,16 +1,27 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.catdetail
 
 import android.content.Context
 import android.net.Uri
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
@@ -38,8 +49,8 @@ fun Player(modifier: Modifier) {
         val source = ProgressiveMediaSource.Factory(dataSourceFactory)
             .createMediaSource(
                 Uri.parse(
-                   "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-                 )
+                    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                )
             )
         player.prepare(source)
         player.playWhenReady = autoPlay
@@ -47,14 +58,9 @@ fun Player(modifier: Modifier) {
         player
     }
 
-
-
     val playerView = remember {
-         PlayerView(context)
-
-
+        PlayerView(context)
     }
-
 
     AndroidView(
         factory = { playerView },
